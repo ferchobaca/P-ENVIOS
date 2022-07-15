78db795bd6ce5ea655fe70e385d3a79b3737f2a1
ferchobaca <ferchobaca@yahoo.com.mx>
2022-07-15 03:36:37 -0500

    Update conecctionBD.js

--- Backend/src/config/conecctionBD.js
+++ Backend/src/config/conecctionBD.js
@@ -1,20 +1,9 @@
-/*const mysql = require('mysql');
-const conecta = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'fernando321',
-    port: '3306',
-    database: 'prueba'
-});
-conecta.connect();
-module.exports = conecta;
-*/
 const sql = require('mssql')
 const config = {
-    user: '4geekinguser',
-    password: 'g3KYQCEts$1555',
-    server: '4geeking.com', // You can use 'localhost\\instance' to connect to named instance
-    database: 'penvios',
+    user: 'userserver',
+    password: 'passwordserver',
+    server: 'put a server', // You can use 'localhost\\instance' to connect to named instance
+    database: 'databasename',
     options: {
         encrypt: false, // for azure
         trustServerCertificate: false // change to true for local dev / self-signed certs
